36e39fe9bacdebb8f4412d689910760a84025e66
Mo <mo@Ameals-MacBook-Air.local>
2019-09-05 21:36:21 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,13 +1,38 @@
 console.log("Up and running!");
+
 var cards = ["queen", "queen", "king", "king"];
+
 var cardsInPlay = []
-var cardOne=[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen");
-var cardTwo=[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king");
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
 if (cardsInPlay.length === 2) { 
 console.log("you have selected two cards"); 
 if (cardsInPlay[0] === cardsInPlay[1]) alert("You found a match!"); else alert("Sorry, try again"); 
 }
+
+var cardsInPlay = [];
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+};
+
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	console.log("User flipped" + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);	
+};
